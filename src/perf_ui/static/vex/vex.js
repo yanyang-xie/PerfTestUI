aeb05f03ff6dcd309d8d290bcc8dad9f6b276d77
Yanyang <yanyang.xie@thistech.com>
2016-11-01 14:37:34 +0800

    finish amchart

--- src/perf_ui/static/vex/vex.js
+++ src/perf_ui/static/vex/vex.js
@@ -1,13 +1,11 @@
 
-function drawAmCharts(chartID, columDatas) {
+function drawAmCharts(chartID, columDatas, category_field, category_axis_title, value_axis_titile, graph_color_field, graph_value_field) {
     var chart;
-    
-    alert(chartID);
     
     // SERIAL CHART
     chart = new AmCharts.AmSerialChart();
     chart.dataProvider = columDatas;
-    chart.categoryField = "ResponseTime";
+    chart.categoryField = category_field;
     chart.startDuration = 1;
 
     // AXES
@@ -18,19 +16,19 @@ function drawAmCharts(chartID, columDatas) {
     categoryAxis.fillAlpha = 1;
     categoryAxis.fillColor = "#FAFAFA";
     categoryAxis.gridPosition = "start";
-    categoryAxis.title = "client response time distribution"
+    categoryAxis.title = category_axis_title;
 
     // value
     var valueAxis = new AmCharts.ValueAxis();
     valueAxis.dashLength = 5;
-    valueAxis.title = "client number of response time";
+    valueAxis.title = value_axis_titile;
     valueAxis.axisAlpha = 0;
     chart.addValueAxis(valueAxis);
 
     // GRAPH
     var graph = new AmCharts.AmGraph();
-    graph.valueField = "Client";
-    graph.colorField = "color";
+    graph.valueField = graph_value_field;
+    graph.colorField = graph_color_field;
     graph.balloonText = "<b>[[category]]: [[value]]</b>";
     graph.type = "column";
     graph.lineAlpha = 0;
