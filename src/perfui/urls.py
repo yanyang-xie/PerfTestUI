e541809211a0eae5d4383d9a8f0d91eba25dc981
Yanyang <yanyang.xie@thistech.com>
2016-12-09 17:57:14 +0800

    change index to basic operation

--- src/perfui/urls.py
+++ src/perfui/urls.py
@@ -4,11 +4,12 @@ handler404 = 'perfui.views.page_not_found'
 handler500 = 'perfui.views.page_error'
 
 urlpatterns = patterns('perfui.views',
-    url(r'^$', 'index', name='home'),
+    #url(r'^$', 'index', name='home'),
     url(r'^result/(?P<test_type>.+)', 'show_perf_result', name='perf_result'),
 )
 
 urlpatterns += patterns('perfui.op_views',
+    url(r'^$', 'basic_op_index', name='home'),
     url(r'^op$', 'perf_op_index', name='perf-op-home'),
     url(r'^op/update$', 'update_operation_config', name='update_operation_config'),
     url(r'^op/execute$', 'operation', name='operation'),
